0816ca1ae46fd0f7d18b0eb4bc8d641ac7ad09ac
Danniel Hugo <danniel@stilingue.com.br>
2020-06-06 11:36:13 -0300

    feat: add book save feature

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Book } from './book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  private booksUrl = 'api/books';
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.booksUrl)
+      .pipe(
+        catchError(this.handleError<Book[]>('getBooks', []))
+      );
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.booksUrl, book, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Book>('addBook'))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(operation, error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
--- src/app/book/new-book/new-book.component.ts
+++ src/app/book/new-book/new-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../book';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-book',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewBookComponent implements OnInit {
 
-  constructor() { }
+  model = {
+    title: '',
+    author: '',
+    publisher: '',
+    genre: '',
+  }
+
+  constructor(
+    private bookService: BookService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  addBook(bookForm: NgForm) {
+    this.bookService.addBook(this.model as Book)
+      .subscribe(() => {
+        this.model = {
+          title: '',
+          author: '',
+          publisher: '',
+          genre: '',
+        };
+
+        bookForm.resetForm()
+      });
   }
 
 }
